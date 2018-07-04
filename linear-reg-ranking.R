6ea85b29026146b7a1ac4469ee01f0cdfa49b8f1
kaisinux <kaisinux@gmail.com>
2018-07-04 15:57:22 -0400

    R files for linear regression

---
+++ linear-reg-ranking.R
@@ -0,0 +1,10 @@
+# Importing XLS
+df <- readxl::read_excel("C:/Users/kais.benfredj/Documents/R/Ranking_Tunisia.xlsx")
+
+# Finding the slope and the intercept using linear regression model
+mod <- lm(df$ForGoals ~ df$Points)
+
+
+# Plotting both the data and the linear result
+plot( df$ForGoals, df$Points, ylab = 'Points', xlab = 'For Goals')
+abline(mod, col=2, lwd=4)
