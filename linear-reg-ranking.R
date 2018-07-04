fd428932f265b16884ad112e6eb20555b0b15c07
kaisinux <kaisinux@gmail.com>
2018-07-04 15:57:50 -0400

    Linear regression between Points earned and scored goals

--- linear-reg-ranking.R
+++ linear-reg-ranking.R
@@ -8,3 +8,5 @@ mod <- lm(df$ForGoals ~ df$Points)
 # Plotting both the data and the linear result
 plot( df$ForGoals, df$Points, ylab = 'Points', xlab = 'For Goals')
 abline(mod, col=2, lwd=4)
+
+# End of file
